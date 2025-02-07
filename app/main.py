ccb0dfa4d6e912f060e90a3b30b9122dc1fb7484
Adrianna Regulska <adrianna.regulska.1@gmail.com>
2025-02-07 20:47:31 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -5,11 +5,9 @@ class Cargo:
 
 class BaseRobot:
     def __init__(self, name: str, weight: int, coords: list = None) -> None:
-        if coords is None:
-            coords = [0, 0]
         self.name = name
         self.weight = weight
-        self.coords = coords
+        self.coords = [0, 0] if coords is None else coords
 
     def go_forward(self, step: int = 1) -> None:
         self.coords[1] += step
@@ -29,9 +27,7 @@ class BaseRobot:
 
 class FlyingRobot(BaseRobot):
     def __init__(self, name: str, weight: int, coords: list = None) -> None:
-        super().__init__(name, weight, coords)
-        if coords is None:
-            self.coords = [0, 0, 0]
+        super().__init__(name, weight, [0, 0, 0] if coords is None else coords)
 
     def go_up(self, step: int = 1) -> None:
         self.coords[2] += step
@@ -49,15 +45,20 @@ class DeliveryDrone(FlyingRobot):
                  coords: list = None) -> None:
         super().__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
-        self.current_load = current_load
+        self.current_load = None if current_load is None else current_load
 
     def hook_load(self, cargo: Cargo) -> None:
-        if self.current_load is None:
-            if (isinstance(cargo, Cargo)
-                    and cargo.weight <= self.max_load_weight):
-                self.current_load = cargo
-        else:
-            pass
+        if self.current_load is not None:
+            print("Error: The drone is already carrying a load.")
+            return
+        if not isinstance(cargo, Cargo):
+            print("Error: Invalid cargo type.")
+            return
+        if cargo.weight > self.max_load_weight:
+            print("Error: Cargo weight exceeds the maximum load capacity.")
+            return
+
+        self.current_load = cargo
 
     def unhook_load(self) -> None:
         self.current_load = None
